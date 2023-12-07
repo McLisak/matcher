e76f357d5f75ec758461ed5cd5b5f601088febcc
McLisak <McLisak@gmail.com>
2023-12-07 21:21:44 +0100

    chore: base url update

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://mclisak.github.io/matcher/',
   server: {
     port: 8080,
   },
   build: {
-    outDir: 'docs'
+    outDir: 'docs',
+
   },
   plugins: [react()],
 });
